3035974d8847f47d3caa3ea0ec61ff463d73abb6
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-06-15 13:19:10 +0530

    •Performance fixes

--- app/src/main/java/com/appyware/librariesdemo/MainActivity.java
+++ app/src/main/java/com/appyware/librariesdemo/MainActivity.java
@@ -3,6 +3,10 @@ package com.appyware.librariesdemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.vatsal.imagezoomer.ZoomAnimation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-      /*  final ImageView imageView = (ImageView) findViewById(R.id.iv_launcher);
+        final ImageButton imageView = (ImageButton) findViewById(R.id.ib);
 
         activity = this;
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ZoomAnimation.zoom(imageView, activity);
+                ZoomAnimation zoomAnimation = new ZoomAnimation(activity);
+                zoomAnimation.zoom(v, 600);
             }
-        });*/
+        });
     }
 }
